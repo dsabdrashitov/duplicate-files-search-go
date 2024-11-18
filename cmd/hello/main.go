769ff029f76939a622a0ff3701e1d33b3334f42b
dsabdrashitov <47863341+dsabdrashitov@users.noreply.github.com>
2024-11-19 01:21:37 +0300

    Add ErroneousRow constant.

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/dsabdrashitov/duplicate-files-search-go/pkg/csvdb"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
-	line := "\"hello,\"\"abacaba\", x \"\n"
-	var x string
-	n, err := fmt.Sscanf(line, "%q", &x)
-	fmt.Println(n, err)
-	fmt.Println(x)
+	filename := "data/data.txt"
+	fmt.Printf("Open %q\n", filename)
+	f, err := os.OpenFile(filename, os.O_CREATE, 0777)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, err = f.Seek(64, 0)
+	if err != nil {
+		panic(err)
+	}
+	_, err = f.Write([]byte(csvdb.ErroneousRow))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("done")
 }
--- pkg/csvdb/constants.go
+++ pkg/csvdb/constants.go
@@ -1,5 +1,9 @@
 package csvdb
 
 const (
+	ErroneousRow = ",\"e\"e\x0A" // 6 character string that breakes parsing from any state
+)
+
+const (
 	fileBufferSize = 512
 )
